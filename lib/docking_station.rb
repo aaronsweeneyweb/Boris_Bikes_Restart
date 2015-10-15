c497cefe5cb4cccbcc7b5476eff008fa883e532f
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2015-10-15 18:53:19 +0100

    Refactored code

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,6 +2,8 @@ require_relative 'bike'
 
 class DockingStation
 
+  DEFAULT_CAPACITY = 20
+
   attr_reader :bikes
 
   def initialize
@@ -9,16 +11,27 @@ class DockingStation
   end
 
   def release_bike
-    raise StandardError, 'no bikes available' unless @bikes.count > 0
+    raise StandardError, 'no bikes available' unless !empty?
     @bikes.pop
   end
 
   def dock_bike(bike)
-    raise StandardError, 'docking station is full' unless @bikes.count < 20
+    raise StandardError, 'docking station is full' unless !full?
     @bikes.push bike
   end
 
   def show_bike
     @bikes.last
   end
+
+  private
+
+  def full?
+  @bikes.count < DEFAULT_CAPACITY ? false : true
+  end
+
+  def empty?
+  @bikes.count == 0 #then returns true
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,23 +9,30 @@ describe DockingStation do
     subject.dock_bike(bike)
     expect(subject.release_bike).to be_working
   end
+
   it 'responds to dock_bike' do
     expect(subject).to respond_to(:dock_bike).with(1).argument
   end
+
   it 'responds to show_bike' do
     expect(subject).to respond_to(:show_bike)
   end
+
   it 'shows a bike' do
     b1 = Bike.new
     subject.dock_bike(b1)
     expect(subject.show_bike).to eq b1
   end
+
   it 'raises error when no bikes available' do
     expect { subject.release_bike }.to raise_error 'no bikes available'
   end
+
   it 'raises error when docking_station is full' do
     b1 = Bike.new
-    20.times { subject.dock_bike(b1) }
+    DockingStation::DEFAULT_CAPACITY.times { subject.dock_bike(b1) }
     expect {subject.dock_bike(Bike.new)}.to raise_error 'docking station is full'
   end
+
+
 end
